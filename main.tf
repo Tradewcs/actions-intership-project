f94b1d00302272fdf593df38c18b85e3fd8ca322
Tradewcs <tradewcs@gmail.com>
2024-11-01 08:57:55 +0200

    fix main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = ">= 2.0.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
 provider "docker" {}
 
 variable "dockerhub_username" {
