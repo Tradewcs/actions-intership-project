f34f4156fcdc6fa473787d4ef76c87832f1ce9a3
Tradewcs <tradewcs@gmail.com>
2024-11-01 08:56:04 +0200

    fix main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,62 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.16"
-    }
+provider "docker" {}
+
+variable "dockerhub_username" {
+  type        = string
+  description = "DockerHub Username"
+  default     = ""
+}
+
+variable "dockerhub_password" {
+  type        = string
+  description = "DockerHub Password"
+  sensitive   = true
+  default     = ""
+}
+
+variable "image_name_frontend" {
+  type        = string
+  description = "Frontend Docker image name"
+  default     = "tradewcs/frontend-internship-proj"
+}
+
+variable "image_name_gradle" {
+  type        = string
+  description = "Gradle Docker image name"
+  default     = "tradewcs/gradle-internship-proj"
+}
+
+variable "image_tag" {
+  type        = string
+  description = "Docker image tag"
+  default     = "latest"
+}
+
+resource "null_resource" "docker_login" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      docker login -u "${var.dockerhub_username}" --password-stdin
+    EOT
   }
 }
 
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
+resource "null_resource" "docker_build_push_frontend" {
+  depends_on = [null_resource.docker_login]
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      docker build -f Dockerfile-frontend -t ${var.dockerhub_username}/${var.image_name_frontend}:${var.image_tag} .
+      docker push ${var.dockerhub_username}/${var.image_name_frontend}:${var.image_tag}
+    EOT
+  }
 }
 
-resource "docker_image" "node" {
-  name = "tradewcs/frontend-internship-proj:latest"
-}
+resource "null_resource" "docker_build_push_gradle" {
+  depends_on = [null_resource.docker_login]
 
-resource "docker_container" "node" {
-  image = docker_image.node.name
-  name  = "node_container_github_actions"
-
-  ports {
-    internal = 3000
-    external = 3000
+  provisioner "local-exec" {
+    command = <<-EOT
+      docker build -f Dockerfile-gradle -t ${var.dockerhub_username}/${var.image_name_gradle}:${var.image_tag} .
+      docker push ${var.dockerhub_username}/${var.image_name_gradle}:${var.image_tag}
+    EOT
   }
 }
